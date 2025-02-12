2686c60cb9c44a4162616a6696e9961b5c0ddfc4
Ltaudato1 <gitta.tawer13@gmail.com>
2025-02-12 18:29:38 +0300

    fix bugs + add README.md for lab1

--- lab1/calc.cpp
+++ lab1/calc.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
 #include "grid.hpp"
 
-double getDividedDifference(Point* grid, int nodes) {
-    double result = 0;
-    for (int j = 0; j < nodes; ++j) {
-        double delimiter = 1;
-        for (int i = 0; i < nodes; ++i) {
-            if (i != j) delimiter *= (grid[j].x - grid[i].x);
-        }
-        result += grid[j].y / delimiter;
+double getDividedDifference(Point* grid, int start, int end) {
+    if (end - start == 1) {
+        return (grid[end].y - grid[start].y) / (grid[end].x - grid[start].x);
+    } else {
+        return (getDividedDifference(grid, start+1, end) - getDividedDifference(grid, start, end - 1)) / (grid[end].x - grid[start].x);
     }
-    return result;
 }
 
 double getPolynomValue(Point* grid, double x, int nodes) {
     double result = grid[0].y;
+
     for (int i = 1; i < nodes; ++i) {
-        double dividedDifference = getDividedDifference(grid, i+1);
-        double product = 1;
-        for (int j = 1; j < i; ++j) {
+        double product = getDividedDifference(grid, 0, i);
+        for (int j = 0; j < i; ++j) {
             product *= (x - grid[j].x);
         }
-        result += dividedDifference * product;
+        result += product;
     }
+
     return result;
 }
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,7 +6,7 @@
 
 int main() {
     Point *grid {new Point[10]};
-    getUniformGrid(grid, smoothFunction, 10, -4);
+    getChebyshevGrid(grid, smoothFunction, 10);
     for (int i = 0; i < 10; ++i) {
         printf("x = %.15f\npolynom = %.15f\nactual = %.15f\n\n", grid[i].x, getPolynomValue(grid, grid[i].x, 10), smoothFunction(grid[i].x));
     }
