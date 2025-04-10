f7a3cbde287eb86a96323167fe45727f82d3826e
Ltaudato1 <gitta.tawer13@gmail.com>
2025-04-10 13:39:31 +0300

    bugfix in rk4

--- lab5/calc.cpp
+++ lab5/calc.cpp
@@ -10,16 +10,15 @@ vector<pair<double, double>> runge_cutta4(double (*equation) (double, double), d
     double h = (rightBound - leftBound) / partitions;
 
     for (int k = 1; k <= partitions; ++k) {
-        double x_next = answer[0].first + k * h; 
         double x_prev = answer[k-1].first;
         double y_prev = answer[k-1].second;
 
         double k1 = equation(x_prev, y_prev);
-        double k2 = 2 * equation(x_prev + h / 2, y_prev + h * k1 / 2);
-        double k3 = 2 * equation(x_prev + h / 2, y_prev + h * k2 / 2);
+        double k2 = equation(x_prev + h / 2, y_prev + h * k1 / 2);
+        double k3 = equation(x_prev + h / 2, y_prev + h * k2 / 2);
         double k4 = equation(x_prev + h, y_prev + h * k3);
 
-        answer.push_back({x_next, y_prev + h * (k1 + k2 + k3 + k4) / 6});
+        answer.push_back({x_prev + h, y_prev + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6});
     }
 
     return answer;
